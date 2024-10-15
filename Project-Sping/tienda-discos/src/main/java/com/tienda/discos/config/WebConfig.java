1e854da53ade68eb7ae4d22e90423e5f61c765df
Gerard-B-dev <gerardchank@gmail.com>
2024-10-15 20:29:53 +0200

    back spring

---
+++ Project-Sping/tienda-discos/src/main/java/com/tienda/discos/config/WebConfig.java
@@ -0,0 +1,35 @@
+// src/config/WebConfig.java
+
+package com.tienda.discos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuración para permitir solicitudes CORS desde el frontend.
+ */
+@Configuration
+public class WebConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
+
